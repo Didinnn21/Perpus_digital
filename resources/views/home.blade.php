9d2453cb130e282c0564363cf12fc89b8866ade0
Tama2502 <tamapratama804@gmail.com>
2025-05-23 15:44:04 +0700

    home 5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,28 +3,28 @@
 @section('title', 'Beranda')
 
 @section('content')
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
+@section('content')
+<div class="banner-area auto-height shadow dark text-light content-top-heading bg-fixed text-normal text-center" style="background-image: url(/perpus/img-1.jpg); height:100%;">
+
+    <div class="item">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="content">
+                        <h1>Perpustakaan LPKIA Bandung</h1>
+                        <br><center><p>Repost by <a href='' title='' target='_blank'>Tama & Didin</a></p></center>
+
+                        <form action="{{ route('home.cari') }}" method="get">
+                            @csrf
+                            <input type="text" placeholder="Cari Judul Buku" class="form-control" name="buku">
+                            <button type="kirim">
+                                <i class="fa fa-search" style="color: white"></i>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</nav>
+</div>
 @endsection
