4f0560f4af99c6a862b1fce7e07ca95dae1b4a77
Tama2502 <tamapratama804@gmail.com>
2025-05-23 15:50:21 +0700

    home 7

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
                     <div class="content">
-                        <h1>Perpustakaan LPKIA Bandung</h1>
+                        <h1 class="text-black">Perpustakaan LPKIA Bandung</h1>
                         <br><center><p>Repost by <a href='' title='' target='_blank'>Tama & Didin</a></p></center>
                     </div>
                 </div>
