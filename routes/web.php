060dd8606b8b06dea8a2d00a85927ed22e886d58
Tama2502 <tamapratama804@gmail.com>
2025-06-30 00:21:39 +0700

    resource webphp

--- routes/web.php
+++ routes/web.php
@@ -40,19 +40,23 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth:member'], 'as' => 'adm
     Route::get('/daftar_buku', [DaftarbukuController::class, 'index'])->name('daftar.buku');
     //Route::get('/peminjaman_buku', [PeminjamanbukuController::class, 'index'])->name('peminjaman.buku');
 
-    Route::get('/bukus', [BukuController::class, 'index'])->name('bukus.index');
-    Route::get('/bukus/create', [BukuController::class, 'create'])->name('bukus.create');
-    Route::post('/bukus', [BukuController::class, 'store'])->name('bukus.store');
-    Route::get('/bukus/{buku}/edit', [BukuController::class, 'edit'])->name('bukus.edit');
-    Route::put('/bukus/{buku}', [BukuController::class, 'update'])->name('bukus.update');
-    Route::delete('/bukus/{id}', [BukuController::class, 'destroy'])->name('bukus.destroy');
+    // Route::get('/bukus', [BukuController::class, 'index'])->name('bukus.index');
+    // Route::get('/bukus/create', [BukuController::class, 'create'])->name('bukus.create');
+    // Route::post('/bukus', [BukuController::class, 'store'])->name('bukus.store');
+    // Route::get('/bukus/{buku}/edit', [BukuController::class, 'edit'])->name('bukus.edit');
+    // Route::put('/bukus/{buku}', [BukuController::class, 'update'])->name('bukus.update');
+    // Route::delete('/bukus/{id}', [BukuController::class, 'destroy'])->name('bukus.destroy');
 
-    Route::get('/members', [MemberController::class, 'index'])->name('members.index');
-    Route::get('/members/create', [MemberController::class, 'create'])->name('members.create');
-    Route::post('/members', [MemberController::class, 'store'])->name('members.store');
-    Route::get('/members/{member}/edit', [MemberController::class, 'edit'])->name('members.edit');
-    Route::put('/members/{member}', [MemberController::class, 'update'])->name('members.update');
-    Route::delete('/members/{member}', [MemberController::class, 'destroy'])->name('members.destroy');
+    Route::resource('/bukus', BukuController::class);
+
+    // Route::get('/members', [MemberController::class, 'index'])->name('members.index');
+    // Route::get('/members/create', [MemberController::class, 'create'])->name('members.create');
+    // Route::post('/members', [MemberController::class, 'store'])->name('members.store');
+    // Route::get('/members/{member}/edit', [MemberController::class, 'edit'])->name('members.edit');
+    // Route::put('/members/{member}', [MemberController::class, 'update'])->name('members.update');
+    // Route::delete('/members/{member}', [MemberController::class, 'destroy'])->name('members.destroy');
+
+    Route::resource('/members', MemberController::class);
 
     Route::get('/daftar-peminjam', [AdminController::class, 'daftarPeminjam'])->name('daftar.peminjam');
 
@@ -60,13 +64,16 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth:member'], 'as' => 'adm
 
 Route::middleware(['auth:member', 'role.member:member'])->group(function () {
     Route::get('/member/dashboard', [MemberController::class, 'dashboard'])->name('member.dashboard');
-    Route::get('/peminjamanbuku', [PeminjamanbukuController::class, 'index'])->name('peminjamanbuku.index');
-    Route::get('/peminjamanbuku/create', [PeminjamanbukuController::class, 'create'])->name('peminjamanbuku.create');
-    Route::post('/peminjamanbuku', [PeminjamanbukuController::class, 'store'])->name('peminjamanbuku.store');
+    // Route::get('/peminjamanbuku', [PeminjamanbukuController::class, 'index'])->name('peminjamanbuku.index');
+    // Route::get('/peminjamanbuku/create', [PeminjamanbukuController::class, 'create'])->name('peminjamanbuku.create');
+    // Route::post('/peminjamanbuku', [PeminjamanbukuController::class, 'store'])->name('peminjamanbuku.store');
 
-    Route::get('/pengembalianbuku', [PeminjamanbukuController::class, 'pengembalianIndex'])->name('pengembalianbuku.index');
-    Route::get('/pengembalianbuku/{id}/form', [PeminjamanbukuController::class, 'formPengembalian'])->name('pengembalianbuku.form');
-    Route::post('/pengembalianbuku/{id}/kembalikan', [PeminjamanbukuController::class, 'kembalikan'])->name('pengembalianbuku.kembalikan');
+    Route::resource('/peminjamanbuku', PeminjamanbukuController::class);
+    Route::resource('/pengembalianbuku', PeminjamanbukuController::class);
+
+    // Route::get('/pengembalianbuku', [PeminjamanbukuController::class, 'pengembalianIndex'])->name('pengembalianbuku.index');
+    // Route::get('/pengembalianbuku/{id}/form', [PeminjamanbukuController::class, 'formPengembalian'])->name('pengembalianbuku.form');
+    // Route::post('/pengembalianbuku/{id}/kembalikan', [PeminjamanbukuController::class, 'kembalikan'])->name('pengembalianbuku.kembalikan');
 
     Route::get('/riwayat-peminjaman', [RiwayatController::class, 'index'])->name('riwayat.peminjaman');
 
