d20551ebd6e75c17b6cfa3a41bca0288d5bc902d
Didinnn21 <d6258113@gmail.com>
2025-07-12 10:46:22 +0700

    update tampilan

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="container">
         <div class="col-lg-12">
-            <p>Copyright © 2025 Dinin & Tama. &nbsp;&nbsp; <a rel="nofollow"
+            <p>Copyright © 2025 Didin & Tama. &nbsp;&nbsp; <a rel="nofollow"
                     href="https://templatemo.com" target="_blank"></a></p>
         </div>
     </div>
